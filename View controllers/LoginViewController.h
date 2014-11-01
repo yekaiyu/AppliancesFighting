7d751d9bb67b545301682498c300a26e18bd4479
scqiu <qiufawen@qq.com>
2014-11-01 22:27:53 +0800

    Add login UI

---
+++ View controllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Appliances
+//
+//  Created by qiufawen on 1/11/14.
+//  Copyright (c) 2014 yekaiyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ View controllers/LoginViewController.m
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.m
+//  Appliances
+//
+//  Created by qiufawen on 1/11/14.
+//  Copyright (c) 2014 yekaiyu. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)Login:(id)sender {
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
